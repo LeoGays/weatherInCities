30afe8a7e0843c792e530414c5b2e6344966f838
Илья Горкун <boricos245@gmail.com>
2019-07-27 01:39:36 +0600

    Make app

---
+++ weather/src/components/Info.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Info = () => (
+    <div>
+        <h2>Погодное приложение</h2>
+        <p>Узнайте приложение в вашем городе!</p>
+    </div>
+);
+
+export default Info;
